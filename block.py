abc70f61fcc2e4af7ecc42b2c0fe1c73bac84eac
Roberto Butti <roberto.butti@gmail.com>
2018-01-11 15:28:06 +0100

    attribute definition of Block class

--- block.py
+++ block.py
@@ -2,22 +2,38 @@ import hashlib as hasher
 import datetime as date
 import uuid
 
-# Define what a Snakecoin block is
+
+# Define what a Schèi block is
 class Block:
+    index = 0
+    timestamp = 0
+    data = None
+    previous_hash = ''
+    hash = ''
+    nonce = None
+
+
     def __init__(self, index, timestamp, data, previous_hash, nonce=None):
         self.index = index
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
-        if (nonce == None):
+        if nonce is None:
             nonce = Block.create_nonce()
         self.nonce = nonce
+
 
     def hash_block(self):
         sha = hasher.sha256()
         # sha.update(str(self.index).encode('utf-8'))
-        sha.update(str(self.index).encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.data).encode('utf-8') + str(self.previous_hash).encode('utf-8')+ str(self.nonce).encode('utf-8'))
+        sha.update(
+            str(self.index).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') +
+            str(self.data).encode('utf-8') +
+            str(self.previous_hash).encode('utf-8') +
+            str(self.nonce).encode('utf-8')
+        )
         return sha.hexdigest()
 
     def create_nonce():
@@ -36,7 +52,6 @@ class Block:
     def create_genesis_block():
         # Manually construct a block with
         # index zero and arbitrary previous hash
-
         return Block(0, date.datetime.now(),
             {
              "proof-of-work": 9,
